d82e787176addd837397adc43904d70a6f1c432a
Evgeny Lobachev <lobachevev@gmail.com>
2021-03-18 21:35:41 +0300

    add more types

--- 1_listTypeCheck.py
+++ 1_listTypeCheck.py
@@ -1,4 +1,4 @@
 if __name__ == '__main__':
-    arr = [2, 2.5, 2 + 4j, 'some string', [1, 2, 3], (1, 2, 3), True]
+    arr = [2, 2.5, 2 + 4j, 'some string', [1, 2, 3], (1, 2, 3), True, b'some string', bytearray(b'some string')]
     for el in arr:
         print(type(el))
