0d227221c3b3f0f97b940e49e8fb60386b5bd7a2
Evgeny Lobachev <lobachevev@gmail.com>
2021-03-18 20:54:32 +0300

    fix formatting

--- 2_secondsToTime.py
+++ 2_secondsToTime.py
@@ -1,10 +1,10 @@
 if __name__ == '__main__':
     timeInSeconds = int(input('Enter time in seconds: '))
     seconds = timeInSeconds % 60
-    minutes = timeInSeconds / 60
+    minutes = timeInSeconds // 60
     hours = 0
     if minutes >= 60:
-        hours = minutes / 60
+        hours = minutes // 60
         minutes = minutes % 60
-    timeString = '{hours:0g}:{minutes:0g}:{seconds:0g}'
+    timeString = '{hours:02g}:{minutes:02g}:{seconds:02g}'
     print(timeString.format(hours=hours, minutes=minutes, seconds=seconds))
