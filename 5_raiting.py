4034208b092329b6d9749e43eed6d111f2fd9a26
Evgeny Lobachev <lobachevev@gmail.com>
2021-03-19 15:24:28 +0300

    rename some variables

--- 5_raiting.py
+++ 5_raiting.py
@@ -3,27 +3,27 @@ from typing import List
 
 class Rating:
     def __init__(self, initial_rating: List[int]):
-        self.__initialRating = sorted(initial_rating, reverse=True)
+        self.__items = sorted(initial_rating, reverse=True)
 
-    def insert(self, new_element: int):
+    def insert(self, new_val: int):
         i = 0
-        while i < len(self.__initialRating):
-            if new_element > self.__initialRating[i]:
-                self.__initialRating.insert(i, new_element)
+        while i < len(self.__items):
+            if new_val > self.__items[i]:
+                self.__items.insert(i, new_val)
                 return
             i += 1
-        self.__initialRating.append(new_element)
+        self.__items.append(new_val)
 
     def __repr__(self):
-        return ', '.join(map(str, self.__initialRating))
+        return ', '.join(map(str, self.__items))
 
     def __str__(self):
-        return ', '.join(map(str, self.__initialRating))
+        return ', '.join(map(str, self.__items))
 
 
 if __name__ == '__main__':
     rating = Rating([1, 5, 7, 8, 6, 1, 2, 5])
     print(f'Current rating: {rating}')
-    newEl = int(input('Enter a new rating: '))
-    rating.insert(newEl)
+    entered_val = int(input('Enter a new rating: '))
+    rating.insert(entered_val)
     print(rating)
