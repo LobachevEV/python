e8b1d960ebd23c0ee246d994cec2b91df408f339
Evgeny Lobachev <lobachevev@gmail.com>
2021-03-23 19:02:55 +0300

    implement the second task

---
+++ 2_namedFuncArgs.py
@@ -0,0 +1,9 @@
+def printUserData(first_name, last_name, birth_year, birth_city, residence_city, email, phone):
+    print(f'{first_name} {last_name} {birth_year} {birth_city} {residence_city} {email} {phone}')
+
+
+if __name__ == '__main__':
+    printUserData(first_name=input('Enter a user first name: '), last_name=input('Enter a user last name: '),
+                  birth_year=input('Enter a user birth year: '), birth_city=input('Enter a user birth city: '),
+                  residence_city=input('Enter a user residence city: '), email=input('Enter a user email: '),
+                  phone=input('Enter a user phone: '))
