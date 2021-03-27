53faa556f9ebb6ea07424585e858335aee451cef
Evgeny Lobachev <lobachevev@gmail.com>
2021-03-27 13:53:01 +0300

    use prod instead of reduce in the sixth

--- 5_list_mult.py
+++ 5_list_mult.py
@@ -1,7 +1,6 @@
-from functools import reduce
-
+from math import prod
 
 if __name__ == '__main__':
     l = [el for el in range(100, 1001) if el % 2 == 0]
     print(l)
-    print(reduce(lambda a, b: a*b, l))
+    print(prod(l))
