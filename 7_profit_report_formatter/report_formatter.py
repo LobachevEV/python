1ee57e9f5d37abb523db74c04b3a262e51439e3b
Evgeny Lobachev <lobachevev@gmail.com>
2021-04-02 14:16:53 +0300

    implement the seventh task

--- 7_profit_report_formatter/report_formatter.py
+++ 7_profit_report_formatter/report_formatter.py
@@ -2,7 +2,6 @@ import json
 from functools import partial
 from operator import lt
 
-
 if __name__ == '__main__':
     firm_profit_dict = {}
     with open('firm_report.txt', encoding='utf-8') as firm_report:
@@ -10,9 +9,10 @@ if __name__ == '__main__':
             name, _, revenue, loss = map(lambda s: s.strip().strip('.'), line.split())
             profit = float(revenue) - float(loss)
             firm_profit_dict[name] = profit
-    positive_profit = list(filter(partial(lt, 0), firm_profit_dict.values()))
-    print(positive_profit)
+
+    gt_then_0 = partial(lt, 0)
+    positive_profit = list(filter(gt_then_0, firm_profit_dict.values()))
     average_profit = sum(positive_profit) / len(positive_profit)
-    common_report = [firm_profit_dict, {'average_profit': average_profit}]
+
     with open("profit_report.json", 'w') as profit_report:
-        json.dump(common_report, profit_report)
+        json.dump([firm_profit_dict, {'average_profit': average_profit}], profit_report)
